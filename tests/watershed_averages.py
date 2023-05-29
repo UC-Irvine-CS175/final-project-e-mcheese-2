c75ba637b23f9a59e11cdc83b3b3134818763d84
JarrodRShepherd <jrshephe@uci.edu>
2023-05-29 00:09:30 -0700

    Found best dilation and erosion parameters

--- tests/watershed_averages.py
+++ tests/watershed_averages.py
@@ -15,42 +15,43 @@ def test_watershed():
         data = dataset[i][0]
         data_norm = norm(data)
         avg_intensity = my_watershed.getAverage(data_norm)
-        cv2.imwrite(f'test_output\\{i}.png', data)
+                
+        #cv2.imwrite(f'test_output\\{i}.png', data)
+
 
         temp = cv2.cvtColor(data_norm, cv2.COLOR_BGR2GRAY)  
         
         ret, thresh = cv2.threshold(temp,avg_intensity + 2,255, cv2.THRESH_BINARY)
-        cv2.imwrite(f'test_output\\{i}_avg.png', thresh)
+        #cv2.imwrite(f'test_output\\{i}_thresh.png', thresh)
 
+        # noise removal
+        kernel = np.ones((3,3),np.uint8)
 
-        # # noise removal
-        # kernel = np.ones((3,3),np.uint8)
-        # #opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
+        # sure background area
+        sure_bg = cv2.dilate(thresh,kernel,iterations=2)
 
-        # # sure background area
-        # sure_bg = cv2.dilate(thresh,kernel,iterations=1)
+        # Finding sure foreground area
+        dist_transform = cv2.distanceTransform(thresh,cv2.DIST_L2,5)
+        ret, sure_fg = cv2.threshold(dist_transform,0.05*dist_transform.max(),255,0)
 
-        # # Finding sure foreground area
-        # dist_transform = cv2.distanceTransform(thresh,cv2.DIST_L2,5)
-        # ret, sure_fg = cv2.threshold(dist_transform,0.2*dist_transform.max(),255,0)
+        # Finding unknown region
+        sure_fg = np.uint8(sure_fg)
+        unknown = cv2.subtract(sure_bg,sure_fg)
 
-        # # Finding unknown region
-        # sure_fg = np.uint8(sure_fg)
-        # unknown = cv2.subtract(sure_bg,sure_fg)
+        # Marker labelling
+        ret, markers = cv2.connectedComponents(sure_fg)
 
-        # # Marker labelling
-        # ret, markers = cv2.connectedComponents(sure_fg)
+        # Add one to all labels so that sure background is not 0, but 1
+        markers = markers+1
 
-        # # Add one to all labels so that sure background is not 0, but 1
-        # markers = markers+1
+        # Now, mark the region of unknown with zero
+        markers[unknown==255] = 0
 
-        # # Now, mark the region of unknown with zero
-        # markers[unknown==255] = 0
+        markers = cv2.watershed(data,markers)
+        data[markers == -1] = [255,0,0]
 
-        # markers = cv2.watershed(data,markers)
-        # data[markers == -1] = [255,0,0]
-
-        # #cv2.imwrite(f'test_output\\{i}_watershed.png', data)
+        cv2.imwrite(f'test_output\\{i}_watershed.png', data)
+        
 
 if __name__ == '__main__':
     csv_file = 'meta.csv'
